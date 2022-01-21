fc7b1779d6c3bdcb45b4e27f5f7c66b8b3763b29
Анатолий Викторович Моргаль <natolij19831@gmail.com>
2022-01-21 18:07:16 +0200

    Init Office добавлен цикл "while" и "break" по условию

--- TestOffice/siteOpen.py
+++ TestOffice/siteOpen.py
@@ -1,17 +1,22 @@
 from os import system as s_
 import time
-#from time import sleep as slp_
-# adr=input("Введите адрес сайта")
-adr = "youtube.com"
-time.sleep(2) # пауза 2 секунды
-if "https://" in adr:
-    s_('start ' + adr)
-    print("if")
-elif "www." in adr:
-    adr = 'https://' + adr
-    s_('start ' + adr)
-    print("efif")
-else:
-    adr = 'https://www.' + adr
-    s_('start ' + adr)
-    print("else")
+
+# from time import sleep as slp_
+while True:
+    adr = input("Введите адрес сайта\n")
+    # adr = "youtube.com"
+    time.sleep(2)  # пауза 2 секунды
+    if adr == 'q' or adr == 'exit' or adr == 'quit':
+        print('Proces EXIT')
+        break
+    elif "https://" in adr:
+        s_('start ' + adr)
+        print("if")
+    elif "www." in adr:
+        adr = 'https://' + adr
+        s_('start ' + adr)
+        print("efif")
+    else:
+        adr = 'https://www.' + adr
+        s_('start ' + adr)
+        print("else")
