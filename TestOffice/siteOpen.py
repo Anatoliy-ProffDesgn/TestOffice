b6fbd0c35bc4bde1e2bc03449cc250dbafd7a693
Анатолий Викторович Моргаль <natolij19831@gmail.com>
2022-01-21 16:58:09 +0200

    Init Office time.sleep

--- TestOffice/siteOpen.py
+++ TestOffice/siteOpen.py
@@ -1,17 +1,17 @@
-import os as s
-
+from os import system as s_
+import time
+#from time import sleep as slp_
 # adr=input("Введите адрес сайта")
 adr = "youtube.com"
+time.sleep(2) # пауза 2 секунды
 if "https://" in adr:
-    s.system('start ' + adr)
+    s_('start ' + adr)
     print("if")
-
 elif "www." in adr:
     adr = 'https://' + adr
-    s.system('start ' + adr)
+    s_('start ' + adr)
     print("efif")
-
 else:
     adr = 'https://www.' + adr
-    s.system('start ' + adr)
+    s_('start ' + adr)
     print("else")
