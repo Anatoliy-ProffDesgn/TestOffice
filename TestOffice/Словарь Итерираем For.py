c871c4b07d295ed3e57832fa0d1001b32a9fa9b8
Анатолий Викторович Моргаль <88934135+Anatoliy-ProffDesgn@users.noreply.github.com>
2022-02-20 12:28:35 +0200

    Home

--- TestOffice/Словарь Итерираем For.py
+++ TestOffice/Словарь Итерираем For.py
@@ -10,7 +10,23 @@ for i in price.items():
     print(i)
 
 new = {}
+i = price.items()
+print(i)
+print(list(i))
 for key, value in price.items():
     print(key, value)
-    new[value] = key    # реверс словаря (ключ <=> значение) (поменяли местами)
+    new[value] = key  # реверс словаря (ключ <=> значение) (поменяли местами)
 print(new)
+
+k = price.keys()
+print(k)
+v = price.values()
+print(v)
+print(list(v))
+for v in price.values():
+    print(v)
+
+for i in price.keys():  # тот же цыкл что и в начале но с экономией памяти (вычисл. мощностей)
+    # print(i)  # в i помещаются ключи словаря
+    new_price[i] = round(price[i] * 0.85, 2)  # сделаем скидку и округлим(round) до сотых
+print(new_price)
