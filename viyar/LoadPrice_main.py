ddd9b532ebc305e7e3ad95ec6a48534d70613ddf
Анатолий Викторович Моргаль <88934135+Anatoliy-ProffDesgn@users.noreply.github.com>
2023-03-05 16:40:10 +0200

    Home Viyar URLs in .csv

--- viyar/LoadPrice_main.py
+++ viyar/LoadPrice_main.py
@@ -1,17 +1,31 @@
 from LoadURL import FileUrl_Load as load_file_in_url
+import concurrent.futures
 
 
 # ------------загрузка файлов із сайту---------------------
 # адреси для загрузкі прайсів беремо з URL_list
-def download_price(urls):
-    i = int(0)
-    for url in urls:
-        i += 1
-        print(i, 'із', len(urls), 'Очікуєм')
-        try:
-            load_file_in_url(url, 'newPrice' + str(i) + '.html')
-        except Exception:
-            print('Помилка завантаження URL:', url)
-            continue
-        print('newPrice' + str(i) + '.xls', ' - Завантажено')
+# def download_price(urls):
+#     i = int(0)
+#     for url in urls:
+#         i += 1
+#         print(i, 'із', len(urls), 'Очікуєм')
+#         try:
+#             load_file_in_url(url, 'newPrice' + str(i) + '.html')
+#         except Exception:
+#             print('Помилка завантаження URL:', url)
+#             continue
+#         print('newPrice' + str(i) + '.xls', ' - Завантажено')
 
+
+def download_price(urls):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_url = {executor.submit(load_file_in_url, url, f"newPrice{i}.html"): url for i, url in
+                         enumerate(urls, 1)}
+        for future in concurrent.futures.as_completed(future_to_url):
+            url = future_to_url[future]
+            try:
+                future.result()
+            except Exception as exc:
+                print(f'Помилка завантаження URL {url}: {exc}')
+            else:
+                print(f'Файл для {url} успішно завантажено')
--- viyar/main_Full_Updete_Price.py
+++ viyar/main_Full_Updete_Price.py
@@ -1,8 +1,9 @@
 import json as js
 import datetime as d
+import csv
 import LoadPrice_main as Load_price
-from URL_list import url_list as urls
 from Parse_html import full_price as f_price
+
 
 # up = input('завантажити з сайту (може тривати від декількох хвилин...) Y/n')
 # re = input('Переписати Прайс Y/n')
@@ -23,12 +24,25 @@ def load_update(load_in_url=True, update_in_file=True, file_name=''):
     else:
         price_file_name = file_name
     if load_in_url:
+        urls=open_urls_csv()
         Load_price.download_price(urls)
     if update_in_file:
         with open(price_file_name, 'w', encoding='utf-8') as file:
             price = f_price('./DownloadPrices/')
             js.dump(price, file)
 
+
 # print(prise_file_name)
 
 # load_update(False, True)
+
+def open_urls_csv(file_name='Shablon/URLs.csv'):
+    url_s = []
+
+    # Відкриваємо файл та читаємо url з кожного рядка
+    with open(file_name, newline='', encoding='UTF-8') as csvfile:
+        reader = csv.reader(csvfile, dialect=csv.excel)
+        for row in reader:
+            if row:
+                url_s.append(row[0])
+    return url_s
